fd047a91080aa8c1030cd1411d86a37bd9ae6d8c
MelanieAvz <89859945+MelanieAvz@users.noreply.github.com>
2021-08-31 08:37:20 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(4)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(5)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+})
+basic.showString("Melanie")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+})
